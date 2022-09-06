b73659a668a2790b1fedcfebcf54b18640fe02e0
rofa <roghaye.fallahi75@gmail.com>
2022-09-06 22:58:19 +0430

    Dynamic of category

--- app/Http/Livewire/Index/Index.php
+++ app/Http/Livewire/Index/Index.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Index;
 
 use App\Models\Article;
+use App\Models\Category;
 use Livewire\Component;
 
 class Index extends Component
@@ -10,11 +11,13 @@ class Index extends Component
 
     public $bestArticle;
     public $newArticle;
+    public $categories;
 
     public function mount()
     {
         $this->newArticle = Article::orderBy('id', 'DESC')->take(4)->get();
         $this->bestArticle = Article::where('is_best', 1)->take(4)->get();
+        $this->categories = Category::all();
     }
 
     public function render()
